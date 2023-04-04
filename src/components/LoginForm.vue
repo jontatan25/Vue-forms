5803b7cd530c813f1893fb2a82d6a59b4f4d2a00
Jonathan Sarmiento <jhonn.photo@gmail.com>
2023-04-04 09:17:09 -0500

    feat/ password validation implemented. useForm implemented.

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -8,7 +8,12 @@
         :error="emailError"
       />
 
-      <BaseInput label="Password" type="password" />
+      <BaseInput
+        label="Password"
+        type="password"
+        v-model="password"
+        :error="passwordError"
+      />
 
       <BaseButton type="submit" class="-fill-gradient"> Submit </BaseButton>
     </form>
@@ -16,16 +21,15 @@
 </template>
 
 <script>
-import { useField } from "vee-validate";
+import { useField, useForm } from "vee-validate";
 export default {
   setup() {
     function onSubmit() {
       alert("Submitted");
     }
 
-    const { value: email, errorMessage: emailError } = useField(
-      "email",
-      function (value) {
+    const validations = {
+      email: (value) => {
         if (!value) return "This field is required";
 
         const regex =
@@ -35,13 +39,30 @@ export default {
         }
 
         return true;
-      }
-    );
+      },
+      password: (value) => {
+        const requiredMessage = "This field is required";
+        if (value === undefined || value === null) return requiredMessage;
+        if (!String(value).length) return requiredMessage;
+
+        return true;
+      },
+    };
+
+    useForm({
+      validationSchema: validations,
+    });
+
+    const { value: email, errorMessage: emailError } = useField("email");
+    const { value: password, errorMessage: passwordError } =
+      useField("password");
 
     return {
       onSubmit,
-      email: email,
-      emailError: emailError,
+      email,
+      emailError,
+      password,
+      passwordError,
     };
   },
 };
